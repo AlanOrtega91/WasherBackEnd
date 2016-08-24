e63df739bb884acef53799e445849a207e29dc79
Alan <alan.ortega91@gmail.com>
2016-08-24 11:51:35 -0500

    Refill de productos por url

--- API/Cleaner/Product/RefillProduct/index.php
+++ API/Cleaner/Product/RefillProduct/index.php
@@ -3,17 +3,15 @@ require_once dirname(__FILE__)."/../../../../DBConnect/SafeString.php";
 require_once dirname(__FILE__)."/../../../../DBConnect/Product.php";
 require_once dirname(__FILE__)."/../../../../DBConnect/Cleaner.php";
 
-if (!isset($_POST['token']) || !isset($_POST['productId']))
+if (!isset($_GET['idLavador']) || !isset($_GET['productId']))
   die(json_encode(array("Satus"=>"ERROR missing values")));
 
 
 try{
-  $token = SafeString::safe($_POST['token']);
-  $productId = SafeString::safe($_POST['productId']);
-  $cleaner = new Cleaner();
-  $cleanerInfo = $cleaner->userHasToken($token);
+  $idCleaner = SafeString::safe($_GET['idLavador']);
+  $productId = SafeString::safe($_GET['productId']);
   $product  = new Product();
-  $product->refillProduct($productId,$cleanerInfo['idLavador']);
+  $product->refillProduct($productId,$idCleaner);
   echo json_encode(array("Status"=>"OK"));
 } catch(cleanerHasNoProductsException $e)
 {
